b50eddb66967697a30409be6d270468f3a26db4a
Johnny <odonnell_johnny@yahoo.com>
2018-10-08 11:24:20 -0700

    Example illustrating behavior of const

---
+++ const-char-pointer.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main() {
+    const char* str = "example";
+    cout << str << endl;
+
+    str = "another example";
+    cout << str << endl;
+}
+
