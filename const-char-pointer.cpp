f5ce150cd9e6b5bed754688234d030cee5dc282b
Johnny O'Donnell <odonnell_johnny@yahoo.com>
2019-04-14 11:48:07 -0400

    Updated const-char-pointer example with explanation and more test cases

--- const-char-pointer.cpp
+++ const-char-pointer.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 
 
+/*
+ * This SO question is helpful to understand this behavior:
+ * https://stackoverflow.com/questions/3228664/why-am-i-able-to-change-the-contents-of-const-char-ptr
+ *
+ */
+
 using namespace std;
 
 int main() {
@@ -9,5 +15,25 @@ int main() {
 
     str = "another example";
     cout << str << endl;
+
+    const int i = 5;
+    cout << i << endl;
+
+    /*
+     * This throws compile-time error
+     *
+    i = 6;
+    cout << i << endl;
+    */
+
+    const char* const const_str = "const const example";
+    cout << const_str << endl;
+
+    /*
+     * This throws compile-time error
+     *
+    const_str = "another const const example";
+    cout << const_str << endl;
+    */
 }
 
