d9afe5128cbba6050819be7da50efb68e41dcf9b
Johnny O'Donnell <odonnell_johnny@yahoo.com>
2019-04-15 10:05:24 -0400

    Add explanations from StackOverflow

--- move-construction-in-action.cpp
+++ move-construction-in-action.cpp
@@ -50,6 +50,12 @@ int main() {
      * Interesting that a move constrcutor does not get called here
      * This might be due to g++ compiler optmization
      *
+     * See:
+     * https://stackoverflow.com/questions/13125632/when-does-move-constructor-get-called
+     * https://stackoverflow.com/questions/7645985/move-constructor-is-not-getting-called-in-c0x
+     * https://stackoverflow.com/questions/38246823/why-is-the-move-constructor-not-called
+     * https://stackoverflow.com/questions/12953127/what-are-copy-elision-and-return-value-optimization
+     *
      */
     Obj o2(referenceToValue(o));
     o2.incrementAndPrint();
