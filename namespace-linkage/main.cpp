4d18a999f87e8a0c84325945b3d22c955520a995
Johnny O'Donnell <odonnell_johnny@yahoo.com>
2019-04-18 21:36:25 -0400

    Add conclusion to question raised in example

--- namespace-linkage/main.cpp
+++ namespace-linkage/main.cpp
@@ -11,6 +11,17 @@ namespace Name {
  *
  * Asking for help here:
  * https://stackoverflow.com/questions/55755236/is-it-possible-to-include-a-namespaced-class-into-another-namespace
+ *
+ * Conclusion:
+ * It looks like this example accurately reflects the statements made at the
+ * end of section 15.2.5 on p.430. If I'm understanding it correctly, with a
+ * C library we won't experience these linker errors because the linker
+ * won't care about the namespace of a C declaration (i.e. function).
+ * However, it does care for a C++ declaration. Meaning we can't easily port
+ * over a C++ library into a namespace. Though it's not impossible.
+ * For the example on p. 414 we have full control over the library so we
+ * can change the how the class is defined. For example, we can define
+ * the class as its defined in the StackOverflow answer.
  */
 
 int main() {
