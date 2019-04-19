3359dc9eb801db91a9a0a46a3cd1a00b131dd140
Johnny O'Donnell <odonnell_johnny@yahoo.com>
2019-04-18 20:07:57 -0400

    Add sidenote to description

--- namespace-linkage/main.cpp
+++ namespace-linkage/main.cpp
@@ -6,6 +6,8 @@ namespace Name {
  * As mentioned on p. 430, C++ headers cannot be included into a namespace
  * like C headers can
  *
+ * See also p. 414. Somehow the example here is able to include C++ headers
+ * into a namespace
  */
 
 int main() {
